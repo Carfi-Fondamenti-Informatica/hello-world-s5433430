a1d1eb8a9c67e2b4256ac62d2db0e066b8aa7350
s5433430 <115527969+s5433430@users.noreply.github.com>
2022-10-11 08:41:50 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,4 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;      //cout significa "stampa a video"
-    std::cout << "Ciao, Mondo!" << std::endl;       //endl significa inserire una nuova riga in output
-    std::cout << "Hola, Mundo!\n Hallo, Welt!" << std::endl;              //  "\n" puù essere utilizzato al posto di "endl" come carattere di a capo
-    std::cout << "Bonjour le Monde!" ;
     return 0; //viene utilizzato dal programma per comunicare al sistema operativo che il programma è terminato con successo.
 }
