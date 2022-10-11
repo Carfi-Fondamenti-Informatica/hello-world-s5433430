ceace28fc815410633324adaf3e2cf7f7aeea53c
s5433430 <115527969+s5433430@users.noreply.github.com>
2022-10-11 08:45:33 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
-int main() {
-    std::cout << "Hello, World!" << std::endl;      //cout significa "stampa a video"
-    return 0; //viene utilizzato dal programma per comunicare al sistema operativo che il programma è terminato con successo.
+#include <iostream>
+using namespace std;
+
+int main () {
+    cout<<"Hello, World!"<<endl;
+    return 0;
+    
 }
